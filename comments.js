d43081a9bcc64c78d362159ea32d57012646e335
Tran Hai Name <tranhainam12072001@gmail.com>
2023-09-29 10:16:37 +0700

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,35 @@
+// Create web server application
+// To run: npm start
+// To test: http://localhost:3000/comments
+
+// Load the express module
 const express = require('express');
+
+// Create an instance of express
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// Load the body-parser module
+const bodyParser = require('body-parser');
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+// Load the comments module
+const comments = require('./comments');
+
+// Load the cors module
+const cors = require('cors');
+
+// Configure the body-parser middleware
+app.use(bodyParser.json());
+
+// Configure the cors middleware
+app.use(cors());
+
+// Configure the static middleware
+app.use(express.static('public'));
+
+// Configure the comments middleware
+app.use('/comments', comments);
+
+// Create a server that listens on port 3000
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
